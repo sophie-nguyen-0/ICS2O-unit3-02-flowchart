1feeb83fcf6cd77cde237a600a39a791cfb1b5ec
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-31 23:36:35 +0000

    parsefloat

--- js/script.js
+++ js/script.js
@@ -18,9 +18,9 @@ if (navigator.serviceWorker) {
  */
 function myButtonClicked() {
   // input
-  const length = parseInt(document.getElementById("length").value)
-  const width = parseInt(document.getElementById("width").value)
-  const height = parseInt(document.getElementById("height").value)
+  const length = parseFloat(document.getElementById("length").value)
+  const width = parseFloat(document.getElementById("width").value)
+  const height = parseFloat(document.getElementById("height").value)
 
   // process
   const volume = (length * width * height) / 3
