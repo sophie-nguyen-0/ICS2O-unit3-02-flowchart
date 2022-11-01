3ba5963c259ed2bd5feec11fc2b86a8a7142270e
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-11-01 00:00:40 +0000

    tofixed

--- js/script.js
+++ js/script.js
@@ -26,5 +26,5 @@ function myButtonClicked() {
   const volume = (length * width * height) / 3
 
   // output
-  document.getElementById("volume").innerHTML = "volume is: " + volume + " cm"
+  document.getElementById("volume").innerHTML = "volume is: " + volume.toFixed(2) + " cm"
 }
